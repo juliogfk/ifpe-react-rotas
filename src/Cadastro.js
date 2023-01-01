8ac3c71ccc75e2957b17cfb57dc5810df0602f0c
juliogfk <juliogfk1@gmail.com>
2023-01-01 12:11:21 -0300

    primeiro commit

---
+++ src/Cadastro.js
@@ -0,0 +1,47 @@
+import { Container,Image } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import image from './assets/imagem.jpg';
+
+function Cadastro() {
+  return (
+    <Container style={{display:'flex',justfyContent:'center', flexDirection:'row',alignItems:'center', marginLeft:500}}>
+    <Image style={{display:'flex',justifyContent:'center',alignItems:'center'}} src={image} />
+    <Form style={{display:'flex', justifyContent:'center', flexDirection:'column', textAlign:'center', alignItems:'center',border:'solid',padding:10, margin:20}}>
+      <Form.Label style={{fontSize: 35, marginBottom:30}}>Cadastrar Usuário</Form.Label>
+      
+      <Form.Group style={{display:'flex',justifyContent:'center'}} className="mb-3" controlId="formBasicEmail">
+        <Form.Label style={{padding:5}}>Nome:</Form.Label>
+        <Form.Control type="nome" placeholder="" />
+      </Form.Group>
+      
+      <Form.Group style={{display:'flex',justifyContent:'center',margin:20}} className="mb-3" controlId="formBasicEmail">
+        <Form.Label style={{padding:5}}>Email:</Form.Label>
+        <Form.Control type="email" placeholder="" />
+      </Form.Group>
+
+      <Form.Group style={{display:'flex',justifyContent:'center'}} className="mb-3" controlId="formBasicPassword">
+        <Form.Label style={{padding:5}}>Senha:</Form.Label>
+        <Form.Control type="password" placeholder="" />
+      </Form.Group>
+      <Button style={{
+        display:'flex',
+        justifyContent:'center',
+        backgroundColor:'rgb(0, 105, 248)', 
+        color: 'white', 
+        fontSize:15, 
+        margin:20, 
+        padding:10, 
+        width:230,
+        border:'none' 
+      }} 
+      variant="primary" type="submit">
+        Cadastrar
+      </Button>
+    </Form>
+
+    </Container>
+  );
+}
+
+export default Cadastro;
---
+++ src/Inicial.js
@@ -0,0 +1,15 @@
+import { Container,Image } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import image from './assets/imagem2.jpg';
+
+function Inicial() {
+  return (
+    <Container style={{display:'flex',justfyContent:'center', flexDirection:'row',alignItems:'center', marginLeft:500}}>
+      <Button>
+        <Image style={{display:'flex',justifyContent:'center',alignItems:'center'}} src={image} />
+      </Button>
+    </Container>
+  );
+}
+
+export default Inicial;
